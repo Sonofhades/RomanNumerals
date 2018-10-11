2cbf5697feef3b0fd8f8d4d4af8a83d219848758
juhlgren <juhlgren@W10-28.student.yo.oulu.fi>
2018-10-11 15:49:43 +0300

    jooh

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,77 @@
 
-public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
+/* 
+The	symbols	'I',	'X',	'C',	and	'M'	can	be	repeated	at	most	3	times	in	a row.
+The	symbols	'V',	'L',	and	'D'	can	never	be repeated.
+The '1' symbols ('I', 'X', and 'C') can only be subtracted from the 2 next highest values ('IV' and	
+'IX',	'XL'	and	'XC',	'CD'	and 'CM').
+Only	one	subtraction	can	be	made	per	numeral	('XC'	is	allowed,	'XXC'	is not).
+The	'5'	symbols	('V',	'L',	and	'D')	can	never	be subtracted.
+ */
+
+public class RomanNumerals 
+{
+	
+	public int convertToInteger(String romanNum) 
+		{
+		romanNum = romanNum.toUpperCase(); // in case someone sends small letters
+		
 		// To be Implemented
 		return 0;
 		
-	}
+		}
+	
+	public int convertOne(char x)
+		{
+		switch (x)
+			{
+		case 'I' : return 1;
+		case 'V' : return 5;
+		case 'X' : return 10;
+		case 'L' : return 50; 
+		case 'C' : return 100; 
+		case 'D' : return 500;
+		case 'M' : return 1000;
+			}
+		return 0;
+		}
+	
+	public int sumUp(String rNum)
+		{
+		int sum=0;
+		for (int i=0; i<rNum.length(); i++)
+			{
+			sum=sum+convertOne(rNum.charAt(i));			
+			}
+		return sum;
+		}
+	
+	public int threeRow (String x)
+		{
+		char y;
+		int number=0;
+		for (int i=0; i<x.length();i++)
+			{
+			y = x.charAt(i);
+			switch (y)
+				{
+				case 'D' : 
+				case 'V' : 
+				case 'L' : if (y == x.charAt(i+1)) return 1;
+				case 'X' : 
+				case 'I' : 
+				case 'M' : 
+				case 'C' : 
+					for (int j=i+1; j<x.length(); j++)
+						{
+						if (y == x.charAt(j)) number++;
+						}
+					if (number > 2) return 2;
+					}
+				
+			}
+		
+		return 0;
+		}
+	
+	
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -2,11 +2,47 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestRomanNumerals {
+public class TestRomanNumerals 
+{
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void test() 
+		{
+		String year = "MCMLXXXIV";
+		RomanNumerals x = new RomanNumerals();
+		if (x.convertToInteger(year) != 1984) fail("Not yet implemented");
+		}
 
+	@Test
+	public void testIndividualLetters() 
+		{
+		String letters="IVXCLM";
+		RomanNumerals x = new RomanNumerals();
+		for (int i=0; i<6; i++)
+			{
+			// char letter = letters.charAt(i);
+			
+			if (x.convertOne(letters.charAt(i)) == 0) fail("Wrong result for individual letter convert test");
+			}
+		}
+	
+	@Test
+	public void testSum_without_considering_the_places_in_string() 
+		{
+		String year = "MCMLXXXIV";
+		RomanNumerals x = new RomanNumerals();
+		if (x.sumUp(year) != 2186) fail("Did not compute correctly");
+		}
+	
+	@Test
+	public void test_are_there_two_VLD_or_more_than_three_IXCM_in_row() 
+		{
+		String year = "MCMLXXXIV";
+		RomanNumerals x = new RomanNumerals();
+		if (x.threeRow(year) != 0) fail("Something went awry");
+		}
+	
+	
+	
+	
 }
